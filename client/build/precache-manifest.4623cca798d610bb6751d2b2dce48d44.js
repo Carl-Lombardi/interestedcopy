bad1cfa208a6aa38ea8a8feb5fe143eb0fb42fac
sublimerocks86 <carl.h.lombardi@gmail.com>
2019-09-24 01:27:49 -0400

    Routing

---
+++ client/build/precache-manifest.4623cca798d610bb6751d2b2dce48d44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2753829e7ef3b688ff45754a4912de06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7979ee0279f39687331",
+    "url": "/static/css/main.92e0e7b1.chunk.css"
+  },
+  {
+    "revision": "e3b4001f7e1dda2004aa",
+    "url": "/static/js/2.afef8b9f.chunk.js"
+  },
+  {
+    "revision": "e7979ee0279f39687331",
+    "url": "/static/js/main.bfc2aa0d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c2676d2354686076d601528f097db3b0",
+    "url": "/static/media/logo.c2676d23.png"
+  }
+]);
